b5cd66723e104bc8305933989bfae9c38a684e7e
argoc <read@mines.edu>
2022-03-29 22:16:41 -0600

    try for one run

--- app/app/database/database.py
+++ app/app/database/database.py
@@ -5,7 +5,6 @@ import asyncpg
 DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASE_URL = 'postgresql://postgres:postgres@0.0.0.0:5432/dev' if DATABASE_URL is None else DATABASE_URL
 database = databases.Database(DATABASE_URL)
-created = False
 
 async def startup():
     # Start database on app startup.
@@ -29,61 +28,65 @@ def provide_connection() -> databases.Database:
 # will attempt to create; only works if user table not already there
 # otherwise just stops due to the error.
 async def create_database():
-    if (created):
-        print("already ran this")
+    DATABASE_CREATE = os.environ.get('DATABASE_CREATE')
+    os.environ.update('DATABASE_CREATE','done')
+
+    create = True if DATABASE_CREATE is None else False
+    if (not create):
+        print("worker already did this")
+        return
+
+    DATABASE_REGEN = os.environ.get('DATABASE_REGEN')
+    regen = False if DATABASE_REGEN is None else True
+
+    if (regen):
+        print("regenerating database, bye-bye data...")
+        sql = [
+            "DROP TABLE IF EXISTS users CASCADE;",
+            "DROP TABLE IF EXISTS jaminfo CASCADE;",
+            "DROP TABLE IF EXISTS robot CASCADE;",
+            "DROP TABLE IF EXISTS control CASCADE;",
+            ]
     else:
-        created = True
-        DATABASE_REGEN = os.environ.get('DATABASE_REGEN')
-        regen = False if DATABASE_REGEN is None else True
+        sql = []
 
-        if (regen):
-            print("regenerating database, bye-bye data...")
-            sql = [
-                "DROP TABLE IF EXISTS users CASCADE;",
-                "DROP TABLE IF EXISTS jaminfo CASCADE;",
-                "DROP TABLE IF EXISTS robot CASCADE;",
-                "DROP TABLE IF EXISTS control CASCADE;",
-                ]
-        else:
-            sql = []
-        sql2=["CREATE EXTENSION IF NOT EXISTS pgcrypto;",
-                'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";',
+    sql2=["CREATE EXTENSION IF NOT EXISTS pgcrypto;",
+            'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";',
 
-                """CREATE TABLE users (
-                        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-                        username TEXT UNIQUE NOT NULL,
-                        password TEXT UNIQUE NOT NULL
-                );""",
-
-                """CREATE TABLE robot (
+            """CREATE TABLE users (
                     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-                    alias TEXT,
-                    userControlId UUID,
-                    FOREIGN KEY(userControlId) REFERENCES users(id)
-                );""",
+                    username TEXT UNIQUE NOT NULL,
+                    password TEXT UNIQUE NOT NULL
+            );""",
 
-                """CREATE TABLE jaminfo (
-                    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-                    lat DOUBLE PRECISION,
-                    lng DOUBLE PRECISION,
-                    intensity DOUBLE PRECISION,
-                    robotId UUID REFERENCES robot(id));""",
+            """CREATE TABLE robot (
+                id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+                alias TEXT,
+                userControlId UUID,
+                FOREIGN KEY(userControlId) REFERENCES users(id)
+            );""",
 
-                "CREATE INDEX jaminfo_robot_idx ON jaminfo(robotId);",
+            """CREATE TABLE jaminfo (
+                id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+                lat DOUBLE PRECISION,
+                lng DOUBLE PRECISION,
+                intensity DOUBLE PRECISION,
+                robotId UUID REFERENCES robot(id));""",
 
-                """CREATE TABLE control (
-                    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-                    userId UUID,
-                    robotId UUID,
-                    FOREIGN KEY(userId) REFERENCES users(id),
-                    FOREIGN KEY(robotId) REFERENCES robot(id)
-                );"""]
-        sql.extend(sql2)
+            "CREATE INDEX jaminfo_robot_idx ON jaminfo(robotId);",
 
-        try:
-            for s in sql:
-                await database.execute(s)
-            print("database created")
-        except asyncpg.exceptions.DataError:
-            print("database already created")
+            """CREATE TABLE control (
+                id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+                userId UUID,
+                robotId UUID,
+                FOREIGN KEY(userId) REFERENCES users(id),
+                FOREIGN KEY(robotId) REFERENCES robot(id)
+            );"""]
+    sql.extend(sql2)
 
+    try:
+        for s in sql:
+            await database.execute(s)
+        print("database created")
+    except asyncpg.exceptions.DataError:
+        print("database already created")
