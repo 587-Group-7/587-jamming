3c2b9d8fa6764bdf31652e7feaad493067f3f4a9
argoc <read@mines.edu>
2022-03-29 20:55:14 -0600

    add bootstrap db creation

--- app/app/database/database.py
+++ app/app/database/database.py
@@ -1,5 +1,6 @@
 import databases
 import os
+import asyncpg
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASE_URL = 'postgresql://postgres:postgres@0.0.0.0:5432/dev' if DATABASE_URL is None else DATABASE_URL
@@ -13,6 +14,8 @@ async def startup():
     except ConnectionRefusedError:
         print("ERROR:    Could not connect to database.")
         raise 
+    # now make the tables if it's the first time (bootstrap)
+    create_database()
 
 async def shutdown():
     # Stop database on shutdown.
@@ -21,3 +24,46 @@ async def shutdown():
 
 def provide_connection() -> databases.Database:
     return database
+
+# will attempt to create; only works if user table not already there
+# otherwise just stops due to the error.
+def create_database():
+    sql = """CREATE EXTENSION IF NOT EXISTS pgcrypto;
+        CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+        CREATE TABLE user (
+                id UUID default uuid_generate_v4() PRIMARY KEY,
+                username TEXT UNIQUE NOT NULL,
+                password TEXT UNIQUE NOT NULL
+        );
+
+        CREATE TABLE jaminfo (
+            id UUID default uuid_generate_v4() PRIMARY KEY,
+            lat DOUBLE PRECISION,
+            lng DOUBLE PRECISION,
+            intensity DOUBLE PRECISION,
+            robot_id INTEGER REFERENCES robot(id));
+
+        CREATE INDEX jaminfo_robot_idx ON jaminfo(robot_id);
+
+        CREATE TABLE robot (
+            id SERIAL PRIMARY KEY,
+            alias TEXT,
+            userControlId UUID,
+            FOREIGN KEY(userControlId) REFERENCES user(id)
+        );
+
+        CREATE TABLE control (
+            id UUID default uuid_generate_v4() PRIMARY KEY,
+            userId UUID,
+            robotId UUID,
+            FOREIGN KEY(userId) REFERENCES user(id),
+            FOREIGN KEY(robotId) REFERENCES robot(id)
+        );
+        """
+    try:
+        await db.execute(sql)
+        print("database created")
+    except asyncpg.exceptions.DataError:
+        print("database already created")
+
