4571482899a7ac9b30e7fe2c49da63c41078ea24
argoc <read@mines.edu>
2022-03-29 21:27:13 -0600

    sql stmt order matters

--- app/app/database/database.py
+++ app/app/database/database.py
@@ -6,9 +6,6 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASE_URL = 'postgresql://postgres:postgres@0.0.0.0:5432/dev' if DATABASE_URL is None else DATABASE_URL
 database = databases.Database(DATABASE_URL)
 
-DATABASE_REGEN = os.environ.get('DATABASE_REGEN')
-regen = False if DATABASE_REGEN is None else True
-
 async def startup():
     # Start database on app startup.
     try:
@@ -31,17 +28,24 @@ def provide_connection() -> databases.Database:
 # will attempt to create; only works if user table not already there
 # otherwise just stops due to the error.
 async def create_database():
+    DATABASE_REGEN = os.environ.get('DATABASE_REGEN')
+    regen = False if DATABASE_REGEN is None else True
+    print("regen: ",regen)
+
     if (regen):
         sql = ["CREATE EXTENSION IF NOT EXISTS pgcrypto;",
-        'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";',
+            'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";',
             "DROP TABLE IF EXISTS users CASCADE;",
+            "DROP TABLE IF EXISTS jaminfo CASCADE;",
+            "DROP TABLE IF EXISTS robot CASCADE;",
+            "DROP TABLE IF EXISTS control CASCADE;",
+
             """CREATE TABLE users (
                     id UUID default uuid_generate_v4() PRIMARY KEY,
                     username TEXT UNIQUE NOT NULL,
                     password TEXT UNIQUE NOT NULL
             );""",
 
-            "DROP TABLE IF EXISTS jaminfo CASCADE;",
             """CREATE TABLE jaminfo (
                 id UUID default uuid_generate_v4() PRIMARY KEY,
                 lat DOUBLE PRECISION,
@@ -51,7 +55,6 @@ async def create_database():
 
             "CREATE INDEX jaminfo_robot_idx ON jaminfo(robot_id);",
 
-            "DROP TABLE IF EXISTS robot CASCADE;",
             """CREATE TABLE robot (
                 id SERIAL PRIMARY KEY,
                 alias TEXT,
@@ -59,7 +62,6 @@ async def create_database():
                 FOREIGN KEY(userControlId) REFERENCES users(id)
             );""",
 
-            "DROP TABLE IF EXISTS control CASCADE;",
             """CREATE TABLE control (
                 id UUID default uuid_generate_v4() PRIMARY KEY,
                 userId UUID,
